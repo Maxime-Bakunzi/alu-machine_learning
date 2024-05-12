4e3e357607b3408002a5f94473fdd7977e740c67
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-05-12 23:53:10 +0200

    committing

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # alu-machine_learning
+
+This Python script generates a bar chart showing the quantity of different fruits distributed among three persons.
+
+## Usage
+
+1. Make sure you have Python installed on your system.
+2. Clone this repository or download the script file (`https://github.com/Maxime-Bakunzi/alu-machine_learning`).
+3. Run the script using Python:
+
---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+# your code here
+x = student_grades 
+
+z = np.arange(0, 110, 10)
+plt.hist(x ,bins = z, edgecolor= "black")
+plt.xlabel("Grades")
+plt.ylabel("Number of Students")
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+plt.xticks(z)
+plt.title("Project A")
+plt.show()
