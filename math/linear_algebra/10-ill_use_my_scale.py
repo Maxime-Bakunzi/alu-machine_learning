99f3371e625ec9a561b02c1f4644ed09782d278b
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-05-19 21:20:35 +0200

    use my scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+This module defines a function to find shape of an array.
+"""
+
+
+def np_shape(matrix):
+    """
+    Calculate the shape of a numpy.ndarray.
+
+    Args:
+        matrix (numpy.ndarray): The input matrix.
+
+    Returns:
+        tuple: The shape of the matrix as a tuple of integers.
+    """
+    return matrix.shape
