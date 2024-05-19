17254ae936625136aa33bdcc42becedfd763da49
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-05-19 22:24:10 +0200

    the west exchange

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -3,23 +3,15 @@
 This module defines a function transpose matrix.
 """
 
-
 def np_transpose(matrix):
     """
-    Transpose the given matrix.
+    Transposes a numpy.ndarray.
 
     Args:
-        matrix (numpy.ndarray): The input matrix.
+        matrix: A numpy.ndarray.
 
     Returns:
-        numpy.ndarray: The transposed matrix.
+        A new numpy.ndarray which is the transpose of the input matrix.
     """
-    # Reshape the matrix to 2D if possible, otherwise return the matrix itself
-    try:
-        matrix_2d = matrix.reshape(-1, matrix.shape[-1])
-    except ValueError:
-        return matrix
-
-    # Transpose the 2D matrix and reshape it back to the original shape
-    transposed_matrix = matrix_2d.T
-    return transposed_matrix.reshape(matrix.shape[::-1])
+    # Return the transpose of the input matrix
+    return matrix.reshape(matrix.shape[::-1])
