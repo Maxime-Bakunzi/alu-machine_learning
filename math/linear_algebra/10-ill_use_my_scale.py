9cd5d6588dd2d44ae50dbce53a665913c787781c
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-05-19 21:32:13 +0200

    use my scale

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import numpy as np
 """
 This module defines a function to find shape of an array.
 """
@@ -7,12 +6,16 @@ This module defines a function to find shape of an array.
 
 def np_shape(matrix):
     """
-    Calculate the shape of a numpy.ndarray.
+    Calculate the shape of a nested list.
 
     Args:
-        matrix (numpy.ndarray): The input matrix.
+        matrix (list): The input matrix.
 
     Returns:
         tuple: The shape of the matrix as a tuple of integers.
     """
-    return matrix.shape
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0] if len(matrix) > 0 else []
+    return tuple(shape)
