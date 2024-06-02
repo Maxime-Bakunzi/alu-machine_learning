7a0a7efc8add22978447026ad3b722371866d921
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-06-02 17:14:15 +0200

    Add sum of squares calculation task💪🏿💪🏿

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -17,6 +17,5 @@ def summation_i_squared(n):
     """
     if not isinstance(n, int) or n < 1:
         return None
-    
-    return n * (n + 1) * (2 * n + 1) // 6
 
+    return n * (n + 1) * (2 * n + 1) // 6
