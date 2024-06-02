0ca45d37c548eb8dcba168b9ac4a2804dbf1f313
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-06-02 17:10:24 +0200

    Add sum of squares calculation task💪🏿💪🏿

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to calculate the sum of squares
+of the first n natural numbers.
+"""
+
+
+def summation_i_squared(n):
+    """
+    Calculates the sum of squares of integers from 1 to n.
+
+    Args:
+        n (int): The stopping condition.
+
+    Returns:
+        int: The sum of squares if n is a valid number, otherwise None.
+    """
+    if not isinstance(n, int) or n < 1:
+        return None
+    
+    return n * (n + 1) * (2 * n + 1) // 6
+
