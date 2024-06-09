4b26acfca219b3d8d8bfad9bcea06c32eb5a80f9
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-06-09 12:22:26 +0200

    normal CDF👌🏿

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -75,3 +75,31 @@ class Normal:
         coef = 1 / (self.stddev * (2 * pi) ** 0.5)
         exp = e ** (-0.5 * ((x - self.mean) / self.stddev) ** 2)
         return coef * exp
+
+    def cdf(self, x):
+        """
+        Calculates the value of the CDF for a given x-value
+
+        Args:
+            x (float): The x-value
+
+        Returns:
+            float: The CDF value for x
+        """
+        # Constants for the approximation
+        a1 =  0.254829592
+        a2 = -0.284496736
+        a3 =  1.421413741
+        a4 = -1.453152027
+        a5 =  1.061405429
+        p  =  0.3275911
+
+        # Save the sign of x
+        sign = 1 if x >= self.mean else -1
+        x = abs(x - self.mean) / (self.stddev * (2 ** 0.5))
+
+        # A&S formula 7.1.26 approximation for erf
+        t = 1.0 / (1.0 + p * x)
+        y = 1.0 - (((((a5*t+a4)*t)+a3)*t+a2)*t+a1)*t*(2.7182818285**(-x * x))
+
+        return 0.5 * (1.0 + sign * y)
