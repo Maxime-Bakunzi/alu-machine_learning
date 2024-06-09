81e078e20de5a954119bb297ff4776267bdb9e83
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-06-09 13:37:02 +0200

    binomial💪🏿

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-This module contains the Binomial class which represents a binomial distribution.
+Binomial class which represents a binomial distribution.
 """
+
 
 class Binomial:
     """
@@ -10,7 +11,7 @@ class Binomial:
     def __init__(self, data=None, n=1, p=0.5):
         """
         Initializes the Binomial distribution with data, n, and p.
-        
+
         Parameters:
         data (list): A list of data points to estimate the distribution.
         n (int): The number of Bernoulli trials.
@@ -28,7 +29,7 @@ class Binomial:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            
+
             mean = sum(data) / len(data)
             variance = sum((x - mean) ** 2 for x in data) / len(data)
             p_estimate = 1 - (variance / mean)
