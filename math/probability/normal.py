b44a890445699e2a018c9bc80b45433a24ee54b6
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-06-09 08:40:47 +0200

    normal PDF🅿️

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -59,3 +59,19 @@ class Normal:
             float: The x-value of z
         """
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """
+        Calculates the value of the PDF for a given x-value
+
+        Args:
+            x (float): The x-value
+
+        Returns:
+            float: The PDF value for x
+        """
+        pi = 3.1415926536
+        e = 2.7182818285
+        coef = 1 / (self.stddev * (2 * pi) ** 0.5)
+        exp = e ** (-0.5 * ((x - self.mean) / self.stddev) ** 2)
+        return coef * exp
