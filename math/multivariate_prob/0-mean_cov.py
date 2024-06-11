2dd0799fce4c08ed1bee1d850b860747d66e1afa
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-06-11 21:53:10 +0200

    mean and covariance

---
+++ math/multivariate_prob/0-mean_cov.py
@@ -0,0 +1,29 @@
+#!usr/bin/python3
+"""
+This will calculate the mean and covariance
+"""
+
+
+import numpy as np
+
+
+def mean_cov(X):
+    """
+    The function to calculate the mean and covariance.
+    """
+
+    # X = np.ndarray(n, d)
+
+    n, d = X.shape
+
+    if X.ndim != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+
+    mean = np.mean(X, axis=0, keepdims=True)
+
+    cov = ((X - mean).T @ (X - mean)) / (n - 1)
+
+    return mean, cov
---
+++ math/multivariate_prob/README.md
@@ -0,0 +1,8 @@
+# Multivariate Probability
+
+This project contains a function to calculate the mean and covariance matrix of a given dataset.
+
+## Requirements
+
+- Python 3.5
+- numpy 1.15
