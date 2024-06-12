18b8b41ae9e439eea9c0eb811334867149505498
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-06-12 16:54:58 +0200

    multinormal

---
+++ math/multivariate_prob/multinormal.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This module have a class MultiNormal that represents
+ a Multivariate Normal distribution
+"""
+import numpy as np
+
+
+class MultiNormal:
+    """
+    The class represents a Multivariate Normal distribution
+    """
+
+    def __init__(self, data):
+
+        n, d = data.shape
+
+        if not isinstance(data, np.ndarray) or data.ndim != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+
+        mean = np.mean(data, axis=1, keepdims=True)
+
+        covariance = ((mean - data) @ (mean - data).T) / (n - 1)
+
+        self.mean = mean
+        self.cov = covariance
