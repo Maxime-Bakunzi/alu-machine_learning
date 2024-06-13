ead8fea44bf90946a01509733b99c1dd975f21e3
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-06-13 14:10:58 +0200

    multinormal pdf

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -12,7 +12,9 @@ class MultiNormal:
     """
 
     def __init__(self, data):
-
+        """
+        The constructor for finding the mean and covariance.
+        """
         if not isinstance(data, np.ndarray) or data.ndim != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
 
@@ -27,3 +29,26 @@ class MultiNormal:
 
         self.mean = mean
         self.cov = covariance
+
+    def pdf(self, x):
+        """
+        The function to calculates the PDF
+        """
+
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+
+        d, _ = x.shape
+
+        if x.shape != (d, 1):
+            raise ValueError("x must have the shape ({d}, 1)")
+
+        inverse_cov = np.linalg.inv(self.cov)
+        det_cov = np.linalg.det(self.cov)
+        norm_factor = 1.0 / (np.sqrt((2 * np.pi) ** d * det_cov))
+
+        exponent = -0.5 * ((x - self.mean).T @ inverse_cov * (x - self.mean))
+
+        pdf = norm_factor * np.exp(exponent).item()
+
+        return pdf
