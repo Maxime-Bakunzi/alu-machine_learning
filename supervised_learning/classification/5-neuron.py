8ec6b04be29e83057c622a97386f0d63a1a17362
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-09-13 14:27:55 +0200

    neuron gradient descent

---
+++ supervised_learning/classification/5-neuron.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+"""
+This is a module for creating a neuron.
+"""
+
+import numpy as np
+
+
+class Neuron:
+    """
+    Defines a single neuron performing binary classification.
+    """
+
+    def __init__(self, nx):
+        """
+        Class constructor.
+
+        Args:
+            nx (int): The number of input feature to the neuron.
+
+        Raises:
+            TypeError: If nx is not an integer.
+            ValueError: If nx is less than 1.
+        """
+        # input validation
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        # Private attributes
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the weights vector."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for thr bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the activated output."""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates forward propagation using sigmoid activation function.
+
+        Args:
+            X (numpy.ndarray): input data of shape (nx, m).
+
+        Returns:
+            numpy.ndarray: The activated output (A) of the neuron.
+        """
+
+        # Linear part: Z = W.X + b
+        Z = np.matmul(self.__W, X) + self.__b
+
+        # Sigmoid activation function: A = 1 / (1 + exp(-Z))
+        self.__A = 1 / (1 + np.exp(-Z))
+
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculates the cost using logistic regression.
+
+        Args:
+            Y (numpy.ndarray): Correct labels (1, m) for the input data.
+            A (numpy.ndarray): Activated output (1, m) for each example.
+
+        Returns:
+            float: The cost of the model.
+
+        """
+        m = Y.shape[1]  # number of examples
+        # Compute the cost using cross-entropy
+        cost = - (1 / m) * np.sum(Y * np.log(A) + (1 - Y) *
+                                  np.log(1.0000001 - A))
+
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluate the neuron's predictions.
+
+        Args:
+            X (numpy.ndarray): Input data with shape (nx, m).
+            Y (numpy.ndarray): Correct labels with shape (1, m).
+
+        Returns:
+            numpy.ndarray: Predicted labels (1 or 0) with shape (1, m).
+            float: Cost of the network.
+        """
+
+        # Perform forward propagation to get A
+        A = self.forward_prop(X)
+
+        # Make prediction: 1 if A >= 0.5, otherwise 0
+        predictions = np.where(A >= 0.5, 1, 0)
+
+        # Calculate the cost
+        cost = self.cost(Y, A)
+
+        return predictions, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """"Performs one pass of gradient on the neuron.
+
+        Args:
+            X (numpy.ndarray): Input data with shape (nx, m).
+            Y (numpy.ndarray): Correct labels with shape (1, m).
+            A (numpy.ndarray): Activated output of the neuron.
+            alpha (float): Learning rate.
+
+        Updates:
+            __W: The weights vector.
+            __b: The bias scalar.
+        """
+
+        m = Y.shape[1]
+        dZ = A - Y  # Difference between predicted and actual labels
+        dW = (1 / m) * np.dot(dZ, X.T)  # Gradient of the weights
+        db = (1 / m) * np.sum(dZ)  # Gradient of the bias
+
+        # update the weights and bias
+        self.__W = self.__W - alpha * dW
+        self.__b = self.__b - alpha * db
