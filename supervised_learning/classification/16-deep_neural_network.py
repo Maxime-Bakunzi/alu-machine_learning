22dbe3ab487a3034e1ccae641a16872ed6f823ff
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-09-14 11:43:05 +0200

    deep neural network

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,42 +1,49 @@
 #!/usr/bin/env python3
 """
-This module about the deep neural network.
+Module about the deep neural network performing binary classification.
 """
 
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Defines a deep neural network preforming binary classification
+    """
+    Defines a deep neural network performing binary classification
     """
 
     def __init__(self, nx, layers):
         """
-        class constructor
+        Class constructor for DeepNeuralNetwork
+
         Args:
-            nx(int): number of input features
-            layers (list): list representing the number of nodes in 
-            each layer of the network
+            nx (int): number of input features
+            layers (list): list representing the number of nodes in each layer
+
+        Raises:
+            TypeError: If nx is not an integer
+            ValueError: If nx is less than 1
+            TypeError: If layers is not a list or is empty
+            TypeError: If elements in layers are not all positive integers
         """
 
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        
-        if not isinstance(layers, list) or len(layers) == 0:
+        if not isinstance(layers, list) or not layers:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(layer, int) and layer > 0 for layer in layers):
+        if not all(
+                map(lambda layer: isinstance(layer, int) and layer > 0, layers)):
             raise TypeError("layers must be a list of positive integers")
-        
-        self.L = len(layers)  # number of layers
-        self.cache = {}  # intermediary values of the network
-        self.weights = {}  # weights and biases of the network
 
-        # He et al. initialization of weights
-        for l in range (1, self.L + 1):
-            if l == 1:
-                self.weights['W1'] = np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
-            else:
-                self.weights['W{}'.format(l)] = np.random.randn(layers[l - 1], layers[l - 2]) * np.sqrt(2 / layers[l - 2])
-                self.weights['b{}'.format(l)] = np.zeros((layers[l - 1], 1))
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for le in range(1, self.L + 1):
+            layer_size = layers[le - 1]
+            input_size = nx if le == 1 else layers[le - 2]
+
+            self.weights['W' + str(le)] = np.random.randn(
+                layer_size, input_size) * np.sqrt(2 / input_size)
+            self.weights['b' + str(le)] = np.zeros((layer_size, 1))
