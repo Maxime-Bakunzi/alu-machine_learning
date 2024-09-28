d61d19eda50e80381d2c61cc0e48bf24b459569e
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-09-28 17:43:47 +0200

    adam upgraded

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Module containing the implementation of Adam optimization algorithm using
+ TensorFlow
+"""
+
+import tensorflow as tf
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """
+    Creates the training operation for a neural network in tensorflow
+    using the Adam optimization algorithm.
+
+    Args:
+        loss: The loss of the network
+        alpha: The learning rate
+        beta1: The weight used for the first moment
+        beta2: The weight used for the second moment
+        epsilon: A small number to avoid division by zero
+
+    Returns:
+        The Adam optimization operation
+    """
+    optimizer = tf.train.AdamOptimizer(learning_rate=alpha,
+                                       beta1=beta1,
+                                       beta2=beta2,
+                                       epsilon=epsilon)
+    train_op = optimizer.minimize(loss)
+    return train_op
