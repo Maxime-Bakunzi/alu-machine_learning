68268f75377e10427b86e77cdd0254f8b10421a5
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-09-28 09:53:36 +0200

    l2 regularization cost

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -12,7 +12,8 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     Args:
         cost (float): The cost of the network without L2 regularization
         lambtha (float): The regularization parameter
-        weights (dict): A dictionary of the weights and biases of the neural network
+        weights (dict): A dictionary of the weights and biases of the neural
+         network
         L (int): The number of layers in the neural network
         m (int): The number of data points used
 
@@ -21,7 +22,7 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     """
     l2_cost = 0
     for l in range(1, L + 1):
-        l2_cost += np.sum(np.square(weights[f'W{l}']))
+        l2_cost += np.sum(np.square(weights['W{}'.format(l)]))
 
     l2_cost = (lambtha / (2 * m)) * l2_cost
     return cost + l2_cost
