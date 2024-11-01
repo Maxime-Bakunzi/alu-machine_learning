bd34732f9b50ff1b418238b3b2c43d0ec32fb7e9
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-11-01 19:22:06 +0200

    no genre

---
+++ pipeline/databases/9-no_genre.sql
@@ -0,0 +1,5 @@
+-- Lists all shows from hbtn_0d_tvshows that have no genre linked
+SELECT tv_shows.title, tv_shows_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_shows_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_shows_genres.genre_id;
