9ba00aa9001e97506dfd082741d988212a2045e5
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-11-02 12:43:55 +0200

    best band ever

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Lists all Glam rock bands, ranked by their longevity
 SELECT
     band_name,
-    IFNULL(2020 - formed, 0) - IFNULL(IFNULL(split, 2020) - formed, 0) + IFNULL(2020 - formed, 0) as lifespan
+    IFNULL(2020 - formed, 0) - IFNULL(IFNULL(split, 2020) - formed, 0) + IFNULL(2020 - formed, 0) AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', IFNULL(style, '')) > 0
 ORDER BY lifespan DESC;
