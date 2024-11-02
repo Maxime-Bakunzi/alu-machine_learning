2cf39d0576549f3f8df747f63a356d5a5450e08e
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2024-11-02 12:09:22 +0200

    best band ever

--- pipeline/databases/15-fans.sql
+++ pipeline/databases/15-fans.sql
@@ -1,5 +1,5 @@
 -- Ranks counrty origins of bands by number of fans
 SELECT orign, SUM(fans) as nb_fans
-FROM metal_bands
+FROM ab2979f058de215f0f2ae5b052739e76d3c02ac5
 GROUP BY orign
 ORDER BY nb_fans DESC;
