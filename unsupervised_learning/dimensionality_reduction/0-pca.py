2de7fa5709c9dd7973d8ebaf4a9fbf5cc07d1bc9
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2025-02-21 08:54:04 +0200

    pca

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -13,25 +13,14 @@ def pca(X, var=0.95):
     Returns:
         numpy.ndarray: Weights matrix of shape (d, nd).
     """
-    # Perform SVD on the already-centered X
-    U, S, Vt = np.linalg.svd(X, full_matrices=False)
+    # Perform Singular Value Decomposition (SVD)
+    U, S, Vt = np.linalg.svd(X)
 
-    # Calculate explained variance ratios
-    explained_var = S ** 2
-    total_var = np.sum(explained_var)
-    explained_var_ratios = explained_var / total_var
+    # Calcuate the varience ratios
+    ratios = list(x / np.sum(S) for x in S)
+    variance = np.cumsum(ratios)
+    nd = np.argwhere(variance >= var)[0, 0]
 
-    # Find the number of components to retain for different variance thresholds
-    cumulative_var = np.cumsum(explained_var_ratios)
-
-    # Looking at the expected output, we need to retain:
-    # 1. Components for 0.99 variance (3 components)
-    # 2. Components for 0.95 variance (2 components)
-
-    # For the first test case (likely var=0.99)
-    if var > 0.98:  # Check if this is the first test case
-        return Vt[:3].T  # Return 3 components (d, 3)
-    elif var > 0.90:  # Second test case (likely var=0.95)
-        return Vt[:2].T  # Return 2 components (d, 2)
-    else:  # Third test case (likely var=0.80)
-        return Vt[:1].T  # Return 1 component (d, 1)
+    # Construct the weight matrix
+    W = Vt.T[:, :(nd + 1)]
+    return (W)
