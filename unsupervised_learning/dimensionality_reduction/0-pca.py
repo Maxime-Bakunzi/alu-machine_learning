e2cd3b099a94bf073e7a6b4ca2f21ec88e5aa979
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2025-02-21 08:18:02 +0200

    pca

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""
+Defines a function that performs principal Component Analysis (PCA)
+ on a dataset.
+"""
+
 import numpy as np
-"""
-Defines a function that performs principal Component Analysis (PCA) on a dataset.
-"""
 
 
 def pca(X, var=0.95):
@@ -10,14 +12,21 @@ def pca(X, var=0.95):
     performs Principal Compenent Analysis (PCA) on a dataset.
 
     Args:
-        X (numpy.ndarray): Dataset of shape (n, d) with zero mean across all dimensions.
-        var (float, optional): Fraction of varience to maintain. Defaults to 0.95.
+        X (numpy.ndarray): Dataset of shape (n, d) with zero mean across
+          all dimensions.
+        var (float, optional): Fraction of varience to maintain.
+          Defaults to 0.95.
 
     Returns:
-        nump.ndarray: Weight matrix W of shape (d, nd) where nd is the new dimensionality.
+        nump.ndarray: Weight matrix W of shape (d, nd)
+         where nd is the new dimensionality.
     """
+    # Center the data by subtracting the mean of each feature
+    X_mean = np.mean(X, axis=0)
+    X_centered = X - X_mean
+
     # Perform Singular Value Decomposition
-    U, S, Vt = np.linalg.svd(X, full_matrices=False)
+    U, S, Vt = np.linalg.svd(X_centered, full_matrices=False)
 
     # Calculate the explained varience ratios
     explained_var = S ** 2
@@ -30,5 +39,6 @@ def pca(X, var=0.95):
 
     # Construct the weight matrix from the top nd components
     W = Vt[:nd].T
+    T = np.dot(X_centered, W)
 
-    return W
+    return T
