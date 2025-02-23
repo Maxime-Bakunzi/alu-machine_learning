f9918238ae24f61298e3f4b6be75b609192f2864
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2025-02-23 08:27:54 +0200

    pdf

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -31,4 +31,20 @@ def pdf(X, m, S):
             all values in P should have a minimum value of 1e-300
         or None on failure
     """
-    return None
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(m, np.ndarray) or len(m.shape) != 1:
+        return None
+    if not isinstance(S, np.ndarray) or len(S.shape) != 2:
+        return None
+    n, d = X.shape
+    if d != m.shape[0] or d != S.shape[0] or d != S.shape[1]:
+        return None
+    S_det = np.linalg.det(S)
+    S_inv = np.linalg.inv(S)
+    fac = 1 / np.sqrt(((2 * np.pi) ** d) * S_det)
+    X_m = X - m
+    X_m_dot = np.dot(X_m, S_inv)
+    X_m_dot_X_m = np.sum(X_m_dot * X_m, axis=1)
+    P = fac * np.exp(-0.5 * X_m_dot_X_m)
+    return np.maximum(P, 1e-300)
