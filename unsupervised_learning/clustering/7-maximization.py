60bf1fd76d7aa5b91dcc7b3c0170ecb6398a553b
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2025-02-23 08:30:57 +0200

    maximization

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -33,4 +33,20 @@ def maximization(X, g):
                 containing the updated covariance matrices for each cluster
         or None, None, None on failure
     """
-    return None, None, None
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None
+    if not isinstance(g, np.ndarray) or len(g.shape) != 2:
+        return None, None, None
+    n, d = X.shape
+    k = g.shape[0]
+    if n != g.shape[1]:
+        return None, None, None
+    if not np.isclose(np.sum(g, axis=0), 1).all():
+        return None, None, None
+    pi = np.sum(g, axis=1) / n
+    m = np.dot(g, X) / np.sum(g, axis=1)[:, np.newaxis]
+    S = np.zeros((k, d, d))
+    for i in range(k):
+        y = X - m[i]
+        S[i] = np.dot(g[i] * y.T, y) / np.sum(g[i])
+    return pi, m, S
