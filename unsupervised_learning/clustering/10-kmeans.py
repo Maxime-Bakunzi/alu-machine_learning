a24160ece4757a66cba32350ae578d55a91f0797
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2025-02-23 08:35:49 +0200

    another kmeans

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -27,4 +27,14 @@ def kmeans(X, k):
                 containting the index of the cluster in c
                     that each data point belongs to
     """
-    return None, None
+    # Perform K-means clustering
+    kmeans_model = sklearn.cluster.KMeans(n_clusters=k)
+    kmeans_model.fit(X)
+
+    # Extract the centroid means
+    C = kmeans_model.cluster_centers_
+
+    # Get the index of the cluster for each data point
+    clss = kmeans_model.labels_
+
+    return C, clss
